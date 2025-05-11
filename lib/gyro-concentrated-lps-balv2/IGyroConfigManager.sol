32761c4f55c4e7a36b9f66320ea8d97142600e79
Steffen Schuldenzucker <steffen.schuldenzucker@gmail.com>
2025-05-11 13:04:00 +0200

    add view markers

--- lib/gyro-concentrated-lps-balv2/IGyroConfigManager.sol
+++ lib/gyro-concentrated-lps-balv2/IGyroConfigManager.sol
@@ -6,13 +6,13 @@ import {IGyroConfig} from "./IGyroConfig.sol";
 /// @notice Ad-hoc interface for some functions from GyroConfigManager we need
 /// See https://github.com/gyrostable/governance-l2/pull/5
 interface IGyroConfigManager {
-    function config() external returns (IGyroConfig);
+    function config() external view returns (IGyroConfig);
 
     function setPoolConfigUint(address pool, bytes32 key, uint256 value) external;
 
     function unsetPoolConfig(address pool, bytes32 key) external;
 
-    function owner() external returns (address);
+    function owner() external view returns (address);
 
     // GyroConfigManager fallback-delegates to GyroConfig. We add this function from GyroConfig b/c
     // we need it in tests.
