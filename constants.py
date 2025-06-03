cb797984f6a5647ba7a5bd65aafa6471a93ace5b
Steffen Schuldenzucker <steffen.schuldenzucker@gmail.com>
2025-06-03 23:11:39 +0200

    configure gyro permission contracts

--- constants.py
+++ constants.py
@@ -1,6 +1,7 @@
 CONTRACT_ADDRESSES = {
     "base": {
-        "gyro_config_manager": "TODO",
-        "governance_role_manager": "TODO",
+        # NB these are static and the same on all chains _where it is deployed_ (!!)
+        "gyro_config_manager": "0xCb5830e6dBaD1430D6902a846F1b37d4Cfe49b31",
+        "governance_role_manager": "0x0B39C433F591f4faBa2a3E5B2d55ba05DBDEa392",
     }
 }
