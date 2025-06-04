d2c5dbbbfac86e04693bd86fd9a4a21ea8bae2c1
Steffen Schuldenzucker <steffen.schuldenzucker@gmail.com>
2025-06-04 11:08:28 +0200

    deploy updatable rateprovider bal v2 eth/usd on optimism

--- constants.py
+++ constants.py
@@ -3,5 +3,9 @@ CONTRACT_ADDRESSES = {
         # NB these are static and the same on all chains _where it is deployed_ (!!)
         "gyro_config_manager": "0xCb5830e6dBaD1430D6902a846F1b37d4Cfe49b31",
         "governance_role_manager": "0x0B39C433F591f4faBa2a3E5B2d55ba05DBDEa392",
-    }
+    },
+    "optimism": {
+        "governance_role_manager": "0x0B39C433F591f4faBa2a3E5B2d55ba05DBDEa392",
+        "gyro_config_manager": "0xCb5830e6dBaD1430D6902a846F1b37d4Cfe49b31",
+    },
 }
