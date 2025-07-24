467ea7c85447450afce80a8090c005751625c73a
Steffen Schuldenzucker <steffen.schuldenzucker@gmail.com>
2025-07-24 17:55:46 +0200

    forge fmt

--- src/Updatable3CLPOrchestratorBalV2.sol
+++ src/Updatable3CLPOrchestratorBalV2.sol
@@ -50,15 +50,28 @@ contract Updatable3CLPOrchestratorBalV2 is AccessControlDefaultAdminRules {
 
     address internal constant ZERO_ADDRESS = address(0);
 
-    /// @param _feeds The RateProvider's to use for updates. You can pass the zero address for any of these and then the rate is assumed to be 1. This is useful for the numeraire (see the next item).
-    /// @param _ixNumeraire The token index (in _feeds) to be used as the numeraire. This token will _not_ have an associated rateprovider created.
-    /// @param _admin Address to be set for the `DEFAULT_ADMIN_ROLE`, which can set the pool later and manage permissions
-    /// @param _updater Address to be set for the `UPDATER_ROLE`, which can call `.updateToEdge()`. Pass the zero address if you don't want to set an updater yet; the admin can manage roles later.
+    /// @param _feeds The RateProvider's to use for updates. You can pass the zero address for any
+    /// of these and then the rate is assumed to be 1. This is useful for the numeraire (see the
+    /// next item).
+    /// @param _ixNumeraire The token index (in _feeds) to be used as the numeraire. This token will
+    /// _not_ have an associated rateprovider created.
+    /// @param _admin Address to be set for the `DEFAULT_ADMIN_ROLE`, which can set the pool later
+    /// and manage permissions
+    /// @param _updater Address to be set for the `UPDATER_ROLE`, which can call `.updateToEdge()`.
+    /// Pass the zero address if you don't want to set an updater yet; the admin can manage roles
+    /// later.
     /// @param _gyroConfigManager Address of the `GyroConfigManager` that we can use to set swap
     /// fees
     /// @param _governanceRoleManager Address of the `GovernanceRoleManager` that we can use to set
     ///   swap fees.
-    constructor(address[3] memory _feeds, uint256 _ixNumeraire, address _admin, address _updater, address _gyroConfigManager, address _governanceRoleManager) AccessControlDefaultAdminRules(1 days, _admin) {
+    constructor(
+        address[3] memory _feeds,
+        uint256 _ixNumeraire,
+        address _admin,
+        address _updater,
+        address _gyroConfigManager,
+        address _governanceRoleManager
+    ) AccessControlDefaultAdminRules(1 days, _admin) {
         if (_updater != ZERO_ADDRESS) {
             _grantRole(UPDATER_ROLE, _updater);
         }
@@ -75,7 +88,8 @@ contract Updatable3CLPOrchestratorBalV2 is AccessControlDefaultAdminRules {
             if (i == _ixNumeraire) {
                 childRateProviders[i] = SettableRateProvider(ZERO_ADDRESS);
             } else {
-                childRateProviders[i] = new SettableRateProvider(_getRateProviderRate(IRateProvider(_feeds[i])));
+                childRateProviders[i] =
+                    new SettableRateProvider(_getRateProviderRate(IRateProvider(_feeds[i])));
             }
         }
     }
@@ -91,7 +105,7 @@ contract Updatable3CLPOrchestratorBalV2 is AccessControlDefaultAdminRules {
         emit PoolSet(_pool);
     }
 
-    function _getRateProviderRate(IRateProvider rp) view internal returns (uint256) {
+    function _getRateProviderRate(IRateProvider rp) internal view returns (uint256) {
         if (address(rp) == ZERO_ADDRESS) {
             return 1e18;
         }
@@ -105,11 +119,17 @@ contract Updatable3CLPOrchestratorBalV2 is AccessControlDefaultAdminRules {
 
         PH.PoolMetadata memory meta = PH.getPoolMetadata(pool);
 
-        PH.ProtocolFeeSetting memory oldProtocolFees = PH.getPoolProtocolFeeSetting(gyroConfigManager, address(meta.pool));
+        PH.ProtocolFeeSetting memory oldProtocolFees =
+            PH.getPoolProtocolFeeSetting(gyroConfigManager, address(meta.pool));
 
         if (!(oldProtocolFees.isSet && oldProtocolFees.value == 0)) {
             PH.joinPoolAll(meta);
-            PH.setPoolProtocolFeeSetting(gyroConfigManager, governanceRoleManager, address(meta.pool), PH.ProtocolFeeSetting(true, 0));
+            PH.setPoolProtocolFeeSetting(
+                gyroConfigManager,
+                governanceRoleManager,
+                address(meta.pool),
+                PH.ProtocolFeeSetting(true, 0)
+            );
         }
 
         uint256 alpha = _getAlpha(IGyro3CLPPool(address(meta.pool)));
@@ -117,11 +137,13 @@ contract Updatable3CLPOrchestratorBalV2 is AccessControlDefaultAdminRules {
 
         if (!(oldProtocolFees.isSet && oldProtocolFees.value == 0)) {
             PH.exitPoolAll(meta);
-            PH.setPoolProtocolFeeSetting(gyroConfigManager, governanceRoleManager, address(meta.pool), oldProtocolFees);
+            PH.setPoolProtocolFeeSetting(
+                gyroConfigManager, governanceRoleManager, address(meta.pool), oldProtocolFees
+            );
         }
     }
 
-    function _getAlpha(IGyro3CLPPool _pool) view internal returns (uint256) {
+    function _getAlpha(IGyro3CLPPool _pool) internal view returns (uint256) {
         uint256 root3Alpha = _pool.getRoot3Alpha();
         return root3Alpha.pow(3 * uint256(LogExpMath.ONE_18));
     }
@@ -134,10 +156,13 @@ contract Updatable3CLPOrchestratorBalV2 is AccessControlDefaultAdminRules {
         (uint256 ixX, uint256 ixY) = _calcNonNumeraireIndices(ixNumeraire);
 
         // NB the child value for ixNumeraire = delta_z is implicitly always equal to 1.
-        uint256 pXZdelta = feedValues[ixX].divDown(feedValues[ixNumeraire]).divDown(childValues[ixX]);
-        uint256 pYZdelta = feedValues[ixY].divDown(feedValues[ixNumeraire]).divDown(childValues[ixY]);
+        uint256 pXZdelta =
+            feedValues[ixX].divDown(feedValues[ixNumeraire]).divDown(childValues[ixX]);
+        uint256 pYZdelta =
+            feedValues[ixY].divDown(feedValues[ixNumeraire]).divDown(childValues[ixY]);
 
-        (uint256 PXZdelta, uint256 PYZdelta) = BalancerLPSharePricing.relativeEquilibriumPrices3CLP(alpha, pXZdelta, pYZdelta);
+        (uint256 PXZdelta, uint256 PYZdelta) =
+            BalancerLPSharePricing.relativeEquilibriumPrices3CLP(alpha, pXZdelta, pYZdelta);
 
         if (PXZdelta == pXZdelta && PYZdelta == pYZdelta) {
             // This is a correct condition for the pool actually being in range.
@@ -153,21 +178,25 @@ contract Updatable3CLPOrchestratorBalV2 is AccessControlDefaultAdminRules {
         // TODO we should emit some kind of event
     }
 
-    function _getFeedValues() view internal returns (uint256[3] memory feedValues) {
+    function _getFeedValues() internal view returns (uint256[3] memory feedValues) {
         for (uint256 i = 0; i < 3; ++i) {
             feedValues[i] = _getRateProviderRate(IRateProvider(feeds[i]));
         }
     }
 
     // TODO unify code (but need a unified interface too b/c solidity sucks)
-    function _getChildValues() view internal returns (uint256[3] memory childValues) {
+    function _getChildValues() internal view returns (uint256[3] memory childValues) {
         for (uint256 i = 0; i < 3; ++i) {
             childValues[i] = _getRateProviderRate(childRateProviders[i]);
         }
     }
 
     // Indices for assets "x" and "y" if we label the numeraire "z".
-    function _calcNonNumeraireIndices(uint256 _ixNumeraire) pure internal returns (uint256, uint256) {
+    function _calcNonNumeraireIndices(uint256 _ixNumeraire)
+        internal
+        pure
+        returns (uint256, uint256)
+    {
         return ((_ixNumeraire + 1) % 3, (_ixNumeraire + 2) % 3);
     }
 }
